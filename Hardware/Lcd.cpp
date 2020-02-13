68c49cf605d0f5e3070f0cb48a0fbef5c4fe38f6
TxMarcoRx <58686646+TxMarcoRx@users.noreply.github.com>
2020-02-13 22:42:31 +0100

    Update Lcd.cpp

--- Hardware/Lcd.cpp
+++ Hardware/Lcd.cpp
@@ -16,7 +16,7 @@ void Lcd::print(string text, bool clear) {
     if (clear) lcdClear(lcd);
 
     lcdPosition(lcd, 0, 0);
-    lcdPuts(lcd, text);
+    lcdPuts(lcd, text.c_str());
     return;
 }
 
@@ -24,7 +24,7 @@ void Lcd::print(string text, bool clear, int col, int row) {
 
     if (clear) lcdClear(lcd);
     lcdPosition(lcd, col, row);
-    lcdPuts(lcd, text);
+    lcdPuts(lcd, text.c_str());
     return;
 }
 
